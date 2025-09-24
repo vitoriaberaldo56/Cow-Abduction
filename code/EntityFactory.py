066f3f20f6e086454eb66bd09c541e296cf933ec
vitoriaberaldo <vitoriaberaldo56@gmail.com>
2025-09-24 13:11:39 -0300

    Changing the height of the Enemy 1 and 2 Changing the Height of the Player 1

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -26,10 +26,10 @@ class EntityFactory:
                     list_bg.append(Background(f'Level2Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
             case 'Player1':
-                return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
+                return Player('Player1', (10, WIN_HEIGHT / 2 + 100))
             case 'Player2':
                 return Player('Player2', (10, WIN_HEIGHT / 2 + 30))
             case 'Enemy1':
-                return Enemy('Enemy1', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
+                return Enemy('Enemy1', (WIN_WIDTH + 5, random.randint(40, WIN_HEIGHT // 3)))
             case 'Enemy2':
-                return Enemy('Enemy2', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
+                return Enemy('Enemy2', (WIN_WIDTH + 5, random.randint(40, WIN_HEIGHT // 3)))
